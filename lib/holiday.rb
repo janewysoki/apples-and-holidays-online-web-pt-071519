415ac47c20f663ab1803ac6ff359b961c59ab75f
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-28 00:57:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday_name, supplies|
+      puts "  #holiday_name.capitalize"
   end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
