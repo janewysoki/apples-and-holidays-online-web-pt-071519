540e581e4efb41ae0ccd3bbf8db0aa638b14f69d
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-28 00:59:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,6 +77,7 @@ def all_holidays_with_bbq(holiday_hash)
       holiday if item.include?("BBQ")
     end
   end.flatten.compact
+end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
